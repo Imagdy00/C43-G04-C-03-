a31638c1961125de24207eb1e1ecb34732ac959e
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-01-27 12:33:21 +0200

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,239 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num % 3 == 0 && num % 4 == 0 ? "Yes" : "No");
+            #endregion
+
+
+            #region 2
+            //Console.Write("Enter an integer: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num < 0 ? "negative" : "positive");
+            #endregion
+
+
+            #region 3
+            //Console.Write("Enter three integers separated by spaces: ");
+            //string[] inputs = Console.ReadLine().Split();
+            //int num1 = int.Parse(inputs[0]);
+            //int num2 = int.Parse(inputs[1]);
+            //int num3 = int.Parse(inputs[2]);
+            //Console.WriteLine($"Max element = {Math.Max(num1, Math.Max(num2, num3))}");
+            //Console.WriteLine($"Min element = {Math.Min(num1, Math.Min(num2, num3))}");
+            #endregion
+
+            #region 4
+            //Console.Write("Enter an integer: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num % 2 == 0 ? "Even" : "Odd");
+            #endregion
+
+            #region 5
+            //Console.Write("Enter a character: ");
+            //char ch = char.ToLower(Console.ReadLine()[0]);
+            //if ("aeiou".Contains(ch))
+            //    Console.WriteLine("vowel");
+            //else
+            //    Console.WriteLine("consonant");
+            #endregion
+
+            #region 6
+            //Console.Write("Enter a number: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write(i + (i == n ? "" : ", "));
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            #region 7
+            //Console.Write("Enter a number: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.Write(n * i + " ");
+            //}
+            //Console.WriteLine();
+            #endregion
+
+
+            #region 8
+            //Console.Write("Enter a number: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 2; i <= n; i += 2)
+            //{
+            //    Console.Write(i + (i + 2 > n ? "" : " "));
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            #region 9
+            //Console.Write("Enter the base and exponent separated by a space: ");
+            //string[] inputs = Console.ReadLine().Split();
+            //int baseNum = int.Parse(inputs[0]);
+            //int exponent = int.Parse(inputs[1]);
+            //Console.WriteLine($"Output: {Math.Pow(baseNum, exponent)}");
+            #endregion
+
+            #region 10
+            //Console.WriteLine("Enter marks of five subjects separated by spaces:");
+            //string[] inputs = Console.ReadLine().Split();
+            //int[] marks = Array.ConvertAll(inputs, int.Parse);
+            //int total = marks.Sum();
+            //double average = marks.Average();
+            //double percentage = (total / 500.0) * 100;
+            //Console.WriteLine($"Total marks = {total}");
+            //Console.WriteLine($"Average marks = {average:F2}");
+            //Console.WriteLine($"Percentage = {percentage:F2}");
+            #endregion
+
+
+            #region 11
+            //Console.Write("Enter month number (1-12): ");
+            //int month = int.Parse(Console.ReadLine());
+            //int days = month == 2 ? 28 : (month <= 7 ? (month % 2 == 0 ? 30 : 31) : (month % 2 == 0 ? 31 : 30));
+            //Console.WriteLine($"Days in Month: {days}");
+            #endregion
+
+
+            #region 12
+            //Console.WriteLine("Enter two numbers and an operator (+, -, *, /):");
+            //double num1 = double.Parse(Console.ReadLine());
+            //double num2 = double.Parse(Console.ReadLine());
+            //char op = Console.ReadLine()[0];
+            //double result = op switch
+            //{
+            //    '+' => num1 + num2,
+            //    '-' => num1 - num2,
+            //    '*' => num1 * num2,
+            //    '/' => num1 / num2,
+            //    _ => throw new InvalidOperationException("Invalid operator")
+            //};
+            //Console.WriteLine($"Result: {result}");
+            #endregion
+
+
+            #region 13
+            //Console.Write("Enter a string: ");
+            //string input = Console.ReadLine();
+            //char[] reversed = input.ToCharArray();
+            //Array.Reverse(reversed);
+            //Console.WriteLine(new string(reversed));
+            #endregion
+
+
+            #region 14
+            //Console.Write("Enter an integer: ");
+            //int num = int.Parse(Console.ReadLine());
+            //string reversed = new string(num.ToString().ToCharArray().Reverse().ToArray());
+            //Console.WriteLine($"Reversed: {reversed}");
+            #endregion
+
+
+            #region 15
+           
+
+
+            
+            
+                Console.Write("Enter start and end of range: ");
+                string[] inputs = Console.ReadLine().Split();
+                int start = int.Parse(inputs[0]);
+                int end = int.Parse(inputs[1]);
+                for (int i = start; i <= end; i++)
+                {
+                    if (IsPrime(i))
+                        Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            static bool IsPrime(int num)
+            {
+                if (num < 2) return false;
+                for (int i = 2; i * i <= num; i++)
+                {
+                    if (num % i == 0) return false;
+                }
+                return true;
+            }
+
+
+        #endregion
+
+        #region 16
+        //    Console.Write("Enter a number to convert: ");
+        //    int num = int.Parse(Console.ReadLine());
+        //    string binary = "";
+        //    while (num > 0)
+        //    {
+        //        binary = (num % 2) + binary;
+        //        num /= 2;
+        //    }
+        //Console.WriteLine($"Binary: {binary}");
+        #endregion
+
+        #region 17
+
+        //Console.Write("x1 y1: ");
+        //string[] point1 = Console.ReadLine().Split();
+        //double x1 = double.Parse(point1[0]);
+        //double y1 = double.Parse(point1[1]);
+
+        //Console.Write("x2 y2: ");
+        //string[] point2 = Console.ReadLine().Split();
+        //double x2 = double.Parse(point2[0]);
+        //double y2 = double.Parse(point2[1]);
+
+        //Console.Write("x3 y3: ");
+        //string[] point3 = Console.ReadLine().Split();
+        //double x3 = double.Parse(point3[0]);
+        //double y3 = double.Parse(point3[1]);
+
+
+        //double slope1 = (y2 - y1) / (x2 - x1);
+        //double slope2 = (y3 - y2) / (x3 - x2);
+
+
+        //if (slope1 == slope2)
+        //    Console.WriteLine("The points lie on a straight line.");
+        //else
+        //    Console.WriteLine("The points do not lie on a straight line.");
+        #endregion
+
+
+        #region 18
+//        Console.Write("Enter the time taken by the worker (in hours): ");
+//        double time = double.Parse(Console.ReadLine());
+
+//        if (time >= 2 && time <= 3)
+//        {
+//            Console.WriteLine("Highly Efficient");
+//        }
+//        else if (time > 3 && time <= 4)
+//        {
+//            Console.WriteLine("You need to increase your speed.");
+//        }
+//        else if (time > 4 && time <= 5)
+//{
+//    Console.WriteLine("You need training to improve your speed.");
+//}
+//else if (time > 5)
+//{
+//    Console.WriteLine("You are required to leave the company.");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid input. Time must be greater than or equal to 2 hours.");
+//}
+        #endregion
+    }
+}
+
---
+++ DEMO/Program.cs
@@ -0,0 +1,187 @@
+﻿using System.Text;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region String Formating 
+
+            //string name = "John";
+            //int age = 21;
+            //Console.WriteLine($"My name is {name} and I am {age} years old.");
+
+            #endregion
+
+            #region Control Statements
+            //int number = 10;
+            //if (number > 5)
+            //{
+            //    Console.WriteLine("The number is greater than 5.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is 5 or less.");
+            //}
+
+            #endregion
+
+            #region Conditional Statements - If, Switch [Numeric Datatype]
+
+            //int day = 3;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Unknown day");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Conditional Statements - If, Switch [String Datatype]
+            //string color = "Red";
+            //switch (color)
+            //{
+            //    case "Red":
+            //        Console.WriteLine("The color is Red.");
+            //        break;
+            //    case "Blue":
+            //        Console.WriteLine("The color is Blue.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Unknown color.");
+            //        break;
+            //}
+
+            #endregion
+
+
+            #region Switch [Numeric Datatypes (Comparison Operators)]
+            //int number = 10;
+            //switch (number)
+            //{
+            //    case int n when n > 0 && n <= 10:
+            //        Console.WriteLine("The number is between 1 and 10.");
+            //        break;
+            //    case int n when n > 10 && n <= 20:
+            //        Console.WriteLine("The number is between 11 and 20.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The number is out of range.");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Evolution of Switch in C# 7.0
+            //object value = 42;
+            //switch (value)
+            //{
+            //    case int i:
+            //        Console.WriteLine($"The value is an integer: {i}");
+            //        break;
+            //    case string s:
+            //        Console.WriteLine($"The value is a string: {s}");
+            //        break;
+            //    default:
+            //        Console.WriteLine("The value is of an unknown type.");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Evolution of Switch in C# 8.0
+            //int month = 7;
+            //string season = month switch
+            //{
+            //    12 or 1 or 2 => "Winter",
+            //    3 or 4 or 5 => "Spring",
+            //    6 or 7 or 8 => "Summer",
+            //    9 or 10 or 11 => "Fall",
+            //    _ => "Unknown"
+            //};
+            //Console.WriteLine($"The season is {season}.");
+
+            #endregion
+
+
+            #region Loop Statements - For, Foreach
+            //// For Loop
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine($"Current value: {i}");
+            //}
+
+            //// Foreach Loop
+            //string[] names = { "Alice", "Bob", "Charlie" };
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine($"Name: {name}");
+            //}
+
+            #endregion
+
+
+            #region Loop Statements - Do While, While
+
+            //// While Loop
+            //int count = 1;
+            //while (count <= 3)
+            //{
+            //    Console.WriteLine($"Count: {count}");
+            //    count++;
+            //}
+
+            //// Do While Loop
+            //int num = 1;
+            //do
+            //{
+            //    Console.WriteLine($"Number: {num}");
+            //    num++;
+            //} while (num <= 3);
+
+            #endregion
+
+            #region  String
+
+            //string message = "Hello, World!";
+            //Console.WriteLine($"Message Length: {message.Length}");
+            //Console.WriteLine($"Uppercase: {message.ToUpper()}");
+            //Console.WriteLine($"Substring: {message.Substring(7, 5)}");
+
+            #endregion
+
+            #region StringBuilder
+
+
+            //StringBuilder sb = new StringBuilder("Hello");
+            //sb.Append(", World!");
+            //sb.Insert(0, "Welcome: ");
+            //sb.Replace("World", "C# Developer");
+
+            //Console.WriteLine(sb.ToString());
+
+            #endregion
+
+            #region String Methods
+            //string phrase = " C# is amazing! ";
+            //Console.WriteLine($"Trimmed: '{phrase.Trim()}'");
+            //Console.WriteLine($"Contains 'C#': {phrase.Contains("C#")}");
+            //Console.WriteLine($"Starts with 'C#': {phrase.StartsWith("C#")}");
+            //Console.WriteLine($"Replaced: {phrase.Replace("amazing", "powerful")}");
+
+            #endregion
+
+        }
+    }
+}
